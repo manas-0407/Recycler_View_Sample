17055a4f5e77c813de35ef915c72e83b18a2ae93
manas <manasrai1234567@gmail.com>
2023-01-02 15:00:57 +0530

    Second Commit By Manas.

--- app/src/main/java/com/me_manas/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/me_manas/recyclerviewtest/MainActivity.java
@@ -44,3 +44,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+/*
+What I did:
+
+Steps;
+1: layout to show recycler view, here we use RecyclerView Xml
+2: Initialize a variable with id of this recyclerview in
+   main java file.
+3: Set orientation(Layout Manager) of recyclerview at this moment.
+4: Create a new layout file which contains view to be
+   displayed in RecyclerView.(Card view gives a 3D touch)
+5: Now create a structure class for storing data of each
+   single recycler view item(And dont forget constructor to
+   initialize data of class). Also called Model class.
+6: Now createa a data set i.e., an arrayList of the
+   structure class we build in previous step, because RecyclerView
+   will display this list on the screen.
+7: Now create an Adapter cLass, adapter is like a machine
+   performing some task, here what adapter does is that it
+   takes data from data set and using index basing
+   fills the content of recycler view. Basically it
+   fills the content of recycler view.
+8: Adapter class requires a View Holder.What View holder does is
+   that it simply holds data which was in XML form, as a
+   View.
+9: Now Adapter class has its three abstract methods which
+   need to be implemented by us.
+   1. onCreateViewHolder: this is used to create ViewHolders
+      basically that number of view holder as the size of dataset.
+   2. onBindViewHolder: this method set each item on its
+      position in viewHolder i.e., it binds data on its position
+      in viewHolder.
+   3. getItemCount: Just returns the size of data set.
+10: finally everything related to adapter is done.
+12: Just use setadapter method for your recycler view using this
+    Custom adapter's object reference. ;)
+ */
